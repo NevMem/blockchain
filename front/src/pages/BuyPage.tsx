0a1ddd0ffc08fe3c705d1ab4c745d339c4064caf
NevMem <memlolkek@gmail.com>
2022-03-20 19:21:03 +0300

    Something done

--- front/src/pages/BuyPage.tsx
+++ front/src/pages/BuyPage.tsx
@@ -1,15 +1,62 @@
 import WithAppBar from "../components/WithAppBar";
 import { useParams } from "react-router-dom";
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, Paper, CircularProgress } from "@mui/material";
+import companiesService from "../service/CompaniesService";
+import { Company } from "../data/Company";
+import useAsyncRequest, { RequestError, RequestSuccess } from "../utils/async";
+
+const BuyCompanyTokensCard = (props: { company: Company }) => {
+    return (
+        <Stack spacing={2} style={{marginTop: '16px'}}>
+            <Typography variant='h3'>Покупка токенов компании: {props.company.name}</Typography>
+            <Paper variant='outlined' style={{padding: '8px'}}></Paper>
+        </Stack>
+    );
+};
+
+const ErrorBuyCompanyTokensCard = () => {
+    return (
+        <Stack spacing={2} style={{marginTop: '16px'}}>
+            <Typography variant='h3'>Ошибка загрузки компании</Typography>
+            <Paper variant='outlined' style={{padding: '8px'}}>
+                <Typography>Мы уже работаем над исправлением</Typography>
+            </Paper>
+        </Stack>
+    );
+};
+
+const LoadingBuyCompanyTokensCard = () => {
+    return (
+        <Stack spacing={2} style={{marginTop: '16px'}}>
+            <Typography variant='h3'>Загрузка</Typography>
+            <Paper variant='outlined' style={{padding: '24px'}}>
+                <Stack direction='row' justifyContent='space-around'>
+                    <CircularProgress />
+                </Stack>
+            </Paper>
+        </Stack>
+    );
+};
 
 const BuyPageImpl = () => {
     const params = useParams();
-    const id = params.id;
-    return (
-        <Stack>
-            <Typography>{id}</Typography>
-        </Stack>
-    );
+    const id: string = params.id!;
+
+    const request = useAsyncRequest((controller) => {
+        return companiesService.company(id);
+    });
+
+    if (request instanceof RequestError) {
+        return <ErrorBuyCompanyTokensCard />;
+    }
+
+    if (request instanceof RequestSuccess) {
+        return (
+            <BuyCompanyTokensCard company={request.result} />
+        );
+    }
+
+    return <LoadingBuyCompanyTokensCard />;
 };
 
 const BuyPage = () => {
--- front/src/service/CompaniesService.ts
+++ front/src/service/CompaniesService.ts
@@ -1,24 +1,34 @@
 import { Company } from "../data/Company";
 
 class CompaniesService {
+    private companiesImpl = [
+        {
+            id: 'google',
+            name: 'Google',
+            imageUrl: 'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png',
+        },
+        {
+            id: 'twitter',
+            name: 'Twitter',
+            imageUrl: 'https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png',
+        },
+        {
+            id: 'space-x',
+            name: 'SpaceX',
+            imageUrl: 'https://www.freepnglogos.com/uploads/astronaut-png/astronaut-space-clip-art-5.png',
+        },
+    ];
+
     companies(): Company[] {
-        return [
-            {
-                id: 'google',
-                name: 'Google',
-                imageUrl: 'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png',
-            },
-            {
-                id: 'twitter',
-                name: 'Twitter',
-                imageUrl: 'https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png',
-            },
-            {
-                id: 'space-x',
-                name: 'SpaceX',
-                imageUrl: 'https://www.freepnglogos.com/uploads/astronaut-png/astronaut-space-clip-art-5.png',
-            },
-        ];
+        return this.companiesImpl;
+    }
+
+    company(id: string): Promise<Company> {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res(this.companiesImpl.find(elem => elem.id === id)!)
+            }, 1500)
+        });
     }
 };
 
