bdb5d48c12d7fb35e879c8ae212b250b3663612d
NevMem <memlolkek@gmail.com>
2022-03-21 02:17:21 +0300

    Better

--- front/src/service/CompaniesService.ts
+++ front/src/service/CompaniesService.ts
@@ -25,7 +25,7 @@ class CompaniesService {
                 () => {
                     res(this.companiesImpl);
                 },
-                1500
+                500
             );
         });
     }
@@ -39,7 +39,7 @@ class CompaniesService {
                 } else {
                     rej(Error("Компания не найдена"));
                 }
-            }, 1500)
+            }, 500)
         });
     }
 };
